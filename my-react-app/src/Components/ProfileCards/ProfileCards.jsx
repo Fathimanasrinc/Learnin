594014ed4b40edd9f365b42fde39e6691186c626
Fathimanasrinc <ftmnasrinc@gmail.com>
2025-12-27 12:21:38 +0530

    error fixed

--- my-react-app/src/Components/ProfileCards/ProfileCards.jsx
+++ my-react-app/src/Components/ProfileCards/ProfileCards.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {profile} from "../../assets/blank.png";
 
 function ProfileCards() {
   return (
     <div className='card'>
         <div className='top-card'>
             <div className='top-left'>
-                <img src={profile}/>
                 HEHEKJK
             </div>
         </div>
